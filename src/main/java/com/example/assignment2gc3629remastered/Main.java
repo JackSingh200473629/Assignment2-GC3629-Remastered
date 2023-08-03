ff5d022e680ad3b03bdccdcfbc50f520ef0287d3
Jack <jack@whiteshark.ca>
2023-08-03 00:46:11 -0400

    wikipidia icon added

--- src/main/java/com/example/assignment2gc3629remastered/Main.java
+++ src/main/java/com/example/assignment2gc3629remastered/Main.java
@@ -3,6 +3,7 @@ package com.example.assignment2gc3629remastered;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,8 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("companies-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Client Companies!");
+        Image icon = new Image("https://en.wikipedia.org/wiki/Icon#/media/File:The_Ladder_of_Divine_Ascent_Monastery_of_St_Catherine_Sinai_12th_century.jpg");
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/assignment2gc3629remastered/SceneChanger.java
+++ src/main/java/com/example/assignment2gc3629remastered/SceneChanger.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,6 +18,10 @@ public class SceneChanger {
         //controller.displayCompanyDetails(viewTitle);
 
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        Image icon = new Image("https://en.wikipedia.org/wiki/Icon#/media/File:The_Ladder_of_Divine_Ascent_Monastery_of_St_Catherine_Sinai_12th_century.jpg");
+        stage.getIcons().add(icon);
+
         stage.setScene(scene);
         stage.show();
     }
